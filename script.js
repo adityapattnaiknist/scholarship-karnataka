d8eb0e8e17d9cd7410d470100dbd1e382d863c35
adityapattnaiknist <97521512+adityapattnaiknist@users.noreply.github.com>
2025-04-23 19:56:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 window.onload = function () {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        function (position) {
-          // Location access granted
-          document.getElementById("redirectBtn").style.display = "inline-block";
-        },
-        function (error) {
-          alert("Location access is required to continue.");
-        }
-      );
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  
-    document.getElementById("redirectBtn").addEventListener("click", function () {
-      window.location.href = "https://www.sitaramjindalfoundation.org/scholarships-for-students-in-bangalore.php";
-    });
-  };
-  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        console.log("you are elligible to apply the post .");
+        // You can do something useful here if you want later
+      },
+      function (error) {
+        console.warn("you are not elligible to view as geographic restrictions");
+      }
+    );
+  } else {
+    console.warn("Restrriction due to server unreachable");
+  }
+
+  document.getElementById("redirectBtn").addEventListener("click", function () {
+    window.location.href = "https://www.sitaramjindalfoundation.org/scholarships-for-students-in-bangalore.php";
+  });
+};
