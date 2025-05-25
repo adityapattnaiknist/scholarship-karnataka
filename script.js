793e041e35b2dbe1b22dc62ea49df496dbd60981
adityapattnaiknist <97521512+adityapattnaiknist@users.noreply.github.com>
2025-05-25 20:08:37 +0530

    script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ document.getElementById('driveLink').addEventListener('click', function (e) {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
 
-        // Optional: Show location info or redirect
-        document.getElementById('locationInfo').textContent =
-          `Location accessed: Latitude ${lat}, Longitude ${lon}`;
+        // // Optional: Show location info or redirect
+        // document.getElementById('locationInfo').textContent =
+        //   `Location accessed: Latitude ${lat}, Longitude ${lon}`;
 
         // Then redirect to the drive link
         window.location.href = "https://drive.google.com/file/d/1cvNy0DgfsK_NY9KoiM8JncwrDkvjQO7J/view?usp=sharing";
